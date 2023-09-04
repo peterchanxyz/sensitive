e185aadebf6bcf1d581bcb9cb7a6ecb8f112880a
pcxyz <120380827+peterchanxyz@users.noreply.github.com>
2023-09-04 16:43:53 +0800

    add sync mutex

--- filter.go
+++ filter.go
@@ -2,17 +2,20 @@ package sensitive
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"regexp"
 	"strings"
+	"sync"
 	"time"
 )
 
 // Filter 敏感词过滤器
 type Filter struct {
+	mu    sync.RWMutex
 	trie  *Trie
 	noise *regexp.Regexp
 }
@@ -27,6 +30,8 @@ func New() *Filter {
 
 // UpdateNoisePattern 更新去噪模式
 func (filter *Filter) UpdateNoisePattern(pattern string) {
+	filter.mu.Lock()
+	defer filter.mu.Unlock()
 	filter.noise = regexp.MustCompile(pattern)
 }
 
@@ -39,6 +44,11 @@ func (filter *Filter) LoadWordDict(path string) error {
 	defer f.Close()
 
 	return filter.Load(f)
+}
+
+// Load common method to add words
+func (filter *Filter) LoadBytes(ba []byte) error {
+	return filter.Load(bytes.NewBuffer(ba))
 }
 
 // LoadNetWordDict 加载网络敏感词字典
@@ -71,6 +81,9 @@ func (filter *Filter) LoadNetWordDictTimeout(url string, allowHtml bool, timeout
 
 // Load common method to add words
 func (filter *Filter) Load(rd io.Reader) error {
+	filter.mu.Lock()
+	defer filter.mu.Unlock()
+
 	buf := bufio.NewReader(rd)
 	for {
 		line, _, err := buf.ReadLine()
@@ -88,47 +101,65 @@ func (filter *Filter) Load(rd io.Reader) error {
 
 // AddWord 添加敏感词
 func (filter *Filter) AddWord(words ...string) {
+	filter.mu.Lock()
+	defer filter.mu.Unlock()
 	filter.trie.Add(words...)
 }
 
 // DelWord 删除敏感词
 func (filter *Filter) DelWord(words ...string) {
+	filter.mu.Lock()
+	defer filter.mu.Unlock()
 	filter.trie.Del(words...)
 }
 
 // Filter 过滤敏感词
 func (filter *Filter) Filter(text string) string {
+	filter.mu.RLock()
+	defer filter.mu.RUnlock()
 	return filter.trie.Filter(text)
 }
 
 // Replace 和谐敏感词
 func (filter *Filter) Replace(text string, repl rune) string {
+	filter.mu.RLock()
+	defer filter.mu.RUnlock()
 	return filter.trie.Replace(text, repl)
 }
 
 // FindIn 检测敏感词
 func (filter *Filter) FindIn(text string) (bool, string) {
-	text = filter.RemoveNoise(text)
+	filter.mu.RLock()
+	defer filter.mu.RUnlock()
+	text = filter.noise.ReplaceAllString(text, "")
 	return filter.trie.FindIn(text)
 }
 
 // FindAll 找到所有匹配词
 func (filter *Filter) FindAll(text string) []string {
+	filter.mu.RLock()
+	defer filter.mu.RUnlock()
 	return filter.trie.FindAll(text)
 }
 
 // Validate 检测字符串是否合法
 func (filter *Filter) Validate(text string) (bool, string) {
-	text = filter.RemoveNoise(text)
+	filter.mu.RLock()
+	defer filter.mu.RUnlock()
+	text = filter.noise.ReplaceAllString(text, "")
 	return filter.trie.Validate(text)
 }
 
 func (filter *Filter) ValidateWithWildcard(text string, wildcard rune) (bool, string) {
-	text = filter.RemoveNoise(text)
+	filter.mu.RLock()
+	defer filter.mu.RUnlock()
+	text = filter.noise.ReplaceAllString(text, "")
 	return filter.trie.ValidateWithWildcard(text, wildcard)
 }
 
 // RemoveNoise 去除空格等噪音
 func (filter *Filter) RemoveNoise(text string) string {
+	filter.mu.RLock()
+	defer filter.mu.RUnlock()
 	return filter.noise.ReplaceAllString(text, "")
 }
